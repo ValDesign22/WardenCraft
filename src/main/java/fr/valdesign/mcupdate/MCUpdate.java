6d4ec339ad787fe133c4a353f62fc6123308774c
ValRedstone <valdorner31@gmail.com>
2022-06-15 15:40:09 +0200

    Initial commit

---
+++ src/main/java/fr/valdesign/mcupdate/MCUpdate.java
@@ -0,0 +1,40 @@
+package fr.valdesign.mcupdate;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MCUpdate.MOD_ID)
+public class MCUpdate
+{
+    public static final String MOD_ID = "mcupdate";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MCUpdate()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
